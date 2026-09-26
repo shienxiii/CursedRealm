289b0d1b71d3804a7bcae6c28fd57e80b9a505a2
Shien Hsu <shienxiii@gmail.com>
2026-09-27 02:30:23 +0900

    interior manager updated. updated interior to work through interiormanager. added y rotation for  spawning interior

--- Assets/Scripts/ProceduralInterior/Interior.cs
+++ Assets/Scripts/ProceduralInterior/Interior.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using ProceduralInterior.Types;
 using ProceduralInterior.FunctionLib;
+using UnityEngine.Serialization;
 using UnityEngine.Splines;
 
 
@@ -12,55 +13,54 @@ namespace ProceduralInterior
     {
         // Components
         private SplineContainer _areas;
-
+        
+        // Sample and rooms
+        private Vector3Range _span;
         private Sample[,] _grid;
-
-        [SerializeField] private List<Room> _rooms = new List<Room>();
+        private List<Room> _rooms = new List<Room>();
         private List<WallSample> _wallSamples = new List<WallSample>();
-
-        [SerializeField] private Vector3 _start, _end;
-
-        [Header("Debug")]
-        [SerializeField] private bool _drawDebug = false;
-        [SerializeField] private int _splineToDebug = 0;
-
-        public bool applySeed = false;
-        public int roomSeed = 5000;
-        private System.Random _interiorRandom;
-
+        private System.Random _interiorRandom = null;
+        
         /*** PUBLIC PROPERTIES ***/
         public Sample[,] Grid => _grid;
         public List<Room> Rooms => _rooms;
         public List<WallSample> WallSamples => _wallSamples;
-
         public System.Random InteriorRandom => _interiorRandom;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        }
+        #if UNITY_EDITOR
+            [Header("Debug")]
+            [SerializeField] private bool _drawDebug = false;
+            [SerializeField] private int _splineToDebug = 0;
+            public bool applySeed = false;
+            public int debugSeed = 5000;
 
-        [ContextMenu("Initialize Area")]
-        public void InitializeArea()
-        {
-            if (!applySeed)
+            [ContextMenu("Debug Gen")]
+            public void DebugGenerate()
             {
-                System.Random random = new System.Random();
-                roomSeed = random.Next();
+                if (applySeed)
+                    SeedInterior(debugSeed);
+                else
+                    SeedInterior(UnityEngine.Random.Range(0, 1000000));
+                GenerateInterior(_splineToDebug);
             }
-            _interiorRandom = new System.Random(roomSeed);
-
-            InitializeArea(_splineToDebug);
-        }
-
-        public void InitializeArea(int splineIndex)
+        #endif
+        
+        /// <summary>
+        /// Sample the grid based on the selected spline and generate the interior layout, rooms, and walls
+        /// </summary>
+        /// <param name="splineIndex">index of the spline to use, defaults to 0</param>
+        /// <returns>List of indexes of valid samples on the grid</returns>
+        public void GenerateInterior(int splineIndex = 0)
         {
+            // Should only be called after seeding
+            if (_interiorRandom == null) return;
+            
             if (_areas == null)
                 _areas = GetComponent<SplineContainer>();
 
             ProceduralInteriorSettings settings = InteriorManager.Settings;
-
-            if (!settings || _areas.Splines.Count == 0 || splineIndex >= _areas.Splines.Count) return;
+            
+            if (!settings || _areas.Splines.Count == 0 || splineIndex < 0 || splineIndex >= _areas.Splines.Count) return;
 
             // clear all room and wall information
             _rooms.Clear();
@@ -69,40 +69,38 @@ namespace ProceduralInterior
             // Calculate the area that covers the samples
             Spline area = _areas.Splines[splineIndex];
             if (area.Count == 0) return;
-
+            
             // Holds the spline points in world space of the area to be initialized
             List<Vector2> polygon = new List<Vector2>();
 
-            _start = _end = _areas.transform.TransformPoint(area[0].Position);
-
+            _span = new Vector3Range(_areas.transform.TransformPoint(area[0].Position));
 
             for (int i = 0; i < area.Count; i++)
             {
                 Vector3 point = _areas.transform.TransformPoint(area[i].Position);
                 polygon.Add(new Vector2(point.x, point.z));
 
-                _start.x = point.x < _start.x ? point.x : _start.x;
-                _start.z = point.z < _start.z ? point.z : _start.z;
-
-                _end.x = point.x > _end.x ? point.x : _end.x;
-                _end.z = point.z > _end.z ? point.z : _end.z;
+                _span.A.x = Mathf.Min(point.x, _span.A.x);
+                _span.A.z = Mathf.Min(point.z, _span.A.z);
+                _span.B.x   = Mathf.Max(point.x, _span.B.x);
+                _span.B.z   = Mathf.Max(point.z, _span.B.z);
             }
 
-            int sizeX = Mathf.CeilToInt((_end.x - _start.x) / settings.SampleDimension.x);
-            int sizeY = Mathf.CeilToInt((_end.z - _start.z) / settings.SampleDimension.x);
+            int sizeX = Mathf.CeilToInt((_span.B.x - _span.A.x) / settings.SampleDimension.x);
+            int sizeY = Mathf.CeilToInt((_span.B.z - _span.A.z) / settings.SampleDimension.x);
 
-            // Create the samples
+            // Create the grid
             _grid = new Sample[sizeX, sizeY];
-
-
+            
             List<Vector2Int> validSamples = new List<Vector2Int>();
+            
             for (int u = 0; u < sizeX; u++)
             {
                 for (int v = 0; v < sizeY; v++)
                 {
                     Vector3 worldPoint = GridPointToWorldPoint(u, v);
 
-                    // if grid point is vaild, create the Sample instance and add the grid point to validSamples
+                    // if grid point is valid, create the Sample instance and add the grid point to validSamples
                     if (SamplerHelperFunctions.IsPointInsidePolygon(polygon, new Vector2(worldPoint.x, worldPoint.z)))
                     {
                         _grid[u, v] = new Sample();
@@ -110,23 +108,28 @@ namespace ProceduralInterior
                     }
                 }
             }
-
+            
             RoomGenerator.GenerateRooms(this, validSamples);
             RoomGenerator.SampleWallAndRoomConnections(this);
             RoomGenerator.SampleDoor(this);
 
             for (int i = 0; i < _rooms.Count; i++)
                 WallGenerator.GenerateWallForRoom(this, i);
+            
+            #if UNITY_EDITOR
+                _splineToDebug = splineIndex;
+            #endif
+            
         }
-
+        
         public Vector3 GridPointToWorldPoint(int x, int z, bool bCenterH = true, bool bCenterV = false)
         {
             ProceduralInteriorSettings settings = InteriorManager.Settings;
 
             if (!settings) return Vector3.zero;
 
-            Vector3 worldPoint = _start;
-
+            Vector3 worldPoint = _span.A;
+            
             if (bCenterH)
             {
                 worldPoint.x += (x * settings.SampleDimension.x) + (settings.SampleDimension.x / 2);
@@ -144,55 +147,63 @@ namespace ProceduralInterior
             return worldPoint;
         }
 
-
-        private void OnDrawGizmos()
+        public void SeedInterior(int inSeed)
         {
-            if (_areas == null)
-                _areas = GetComponent<SplineContainer>();
+            _interiorRandom = new System.Random(inSeed);
+        }
 
-            ProceduralInteriorSettings settings = InteriorManager.Settings;
-
-            if (!settings || (_grid?.Length ?? 0) == 0 || !_drawDebug || _areas.Splines.Count == 0 || _splineToDebug >= _areas.Splines.Count) return;
-
-            foreach (Room room in _rooms)
+        #if UNITY_EDITOR
+            private void OnDrawGizmos()
             {
-                Gizmos.color = room.debugColor;
-                foreach (GridSpan space in room.Spaces)
+                if (_areas == null)
+                    _areas = GetComponent<SplineContainer>();
+
+                ProceduralInteriorSettings settings = InteriorManager.Settings;
+
+                if (!_drawDebug || !settings ||
+                    (_grid?.Length ?? 0) == 0 || _areas.Splines.Count == 0 ||
+                    _splineToDebug < 0 || _splineToDebug >= _areas.Splines.Count) return;
+
+                foreach (Room room in _rooms)
                 {
-                    Vector2Int start = space.A;
-                    Vector2Int end = space.B;
+                    Gizmos.color = room.debugColor;
+                    foreach (GridSpan space in room.Spaces)
+                    {
+                        Vector2Int start = space.A;
+                        Vector2Int end = space.B;
 
-                    Vector3 startPoint = GridPointToWorldPoint(start.x, start.y);
-                    Vector3 endPoint = GridPointToWorldPoint(end.x, end.y);
-                    Vector3 center = (startPoint + endPoint) / 2;
+                        Vector3 startPoint = GridPointToWorldPoint(start.x, start.y);
+                        Vector3 endPoint = GridPointToWorldPoint(end.x, end.y);
+                        Vector3 center = (startPoint + endPoint) / 2;
 
-                    Vector2Int dimension = space.GetDimension();
-                    Vector3 size = new Vector3((settings.SampleDimension.x * dimension.x) - 0.1f, 0.0f, (settings.SampleDimension.x * dimension.y) - 0.1f);
+                        Vector2Int dimension = space.GetDimension();
+                        Vector3 size = new Vector3((settings.SampleDimension.x * dimension.x) - 0.1f, 0.0f, (settings.SampleDimension.x * dimension.y) - 0.1f);
 
-                    Gizmos.DrawCube(center, size);
-                }
+                        Gizmos.DrawCube(center, size);
+                    }
 
-                List<Wall> walls = room.Walls;
-                foreach (Wall wall in walls)
-                {
-                    Gizmos.color = wall.IsDoor ? Color.green : Color.white;
+                    List<Wall> walls = room.Walls;
+                    foreach (Wall wall in walls)
+                    {
+                        Gizmos.color = wall.IsDoor ? Color.green : Color.white;
 
-                    Vector3 size = wall.End - wall.Start;
-                    Vector3 normalized = size.normalized;
+                        Vector3 size = wall.End - wall.Start;
+                        Vector3 normalized = size.normalized;
 
-                    float x = Mathf.Abs(size.x) - (normalized.x * 0.075f) + (normalized.z * 0.05f);
-                    float y = 1.0f;
-                    float z = Mathf.Abs(size.z) - (normalized.z * 0.075f) + (normalized.x * 0.05f);
+                        float x = Mathf.Abs(size.x) - (normalized.x * 0.075f) + (normalized.z * 0.05f);
+                        float y = 1.0f;
+                        float z = Mathf.Abs(size.z) - (normalized.z * 0.075f) + (normalized.x * 0.05f);
 
-                    Vector3 center = wall.Start + wall.End;
-                    center.x /= 2;
-                    center.y += 0.5f;
-                    center.z /= 2;
-                    center += (wall.Normal * 0.1f);
+                        Vector3 center = wall.Start + wall.End;
+                        center.x /= 2;
+                        center.y += 0.5f;
+                        center.z /= 2;
+                        center += (wall.Normal * 0.1f);
 
-                    Gizmos.DrawCube(center, new Vector3(x, y, z));
+                        Gizmos.DrawCube(center, new Vector3(x, y, z));
+                    }
                 }
             }
-        }
+        #endif
     }
 }
--- Assets/Scripts/ProceduralInterior/InteriorManager.cs
+++ Assets/Scripts/ProceduralInterior/InteriorManager.cs
@@ -1,67 +1,79 @@
 using System;
 using System.Collections.Generic;
 using ProceduralInterior;
+using ProceduralInterior.FunctionLib;
 using UnityEngine;
 
-public class InteriorManager : MonoBehaviour
+namespace ProceduralInterior
 {
-    private static InteriorManager _instance = null;
-    private static ProceduralInteriorSettings _settings = null;
-    public static System.Random _random = null;
-    private static SerializableDictionary<Interior, int> _interiorMap;
-
-    public static InteriorManager Instance => _instance;
-    public static ProceduralInteriorSettings Settings => _settings;
-    
-    private void Awake()
+    public class InteriorManager : MonoBehaviour
     {
-        Initialize();
-    }
+        private static InteriorManager _instance = null;
+        private static ProceduralInteriorSettings _settings = null;
+        public static System.Random _random = null;
+        private static SerializableDictionary<Interior, int> _interiorMap;
 
-    private void OnValidate()
-    {
-        Initialize();
-    }
+        public static InteriorManager Instance => _instance;
+        public static ProceduralInteriorSettings Settings => _settings;
 
-    private void Initialize()
-    {
-        if (_instance != null && _instance != this)
+        private void Awake()
         {
-            Destroy(this);
-            return;
+            Initialize();
         }
-        
-        _instance = this;
-        _settings = ProceduralInteriorSettings.TryGetSettings();
-        ReSeed();
-    }
 
-    [ContextMenu("Spawn Interior")]
-    public void SpawnLevel()
-    {
-        if (Settings == null) return;
+        private void OnValidate()
+        {
+            Initialize();
+        }
 
-        List<Interior> prefabs = Settings.InteriorPrefabs;
-        if (prefabs == null || prefabs.Count == 0) return;
-        
-        // Spawning single interior for now
-        int i = _random.Next(0, prefabs.Count);
-        GameObject prefab = prefabs[i].gameObject;
-        SpawnInterior(prefab, Vector3.zero);
-    }
+        private void Initialize()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this);
+                return;
+            }
 
-    private GameObject SpawnInterior(GameObject inPrefab, Vector3 inPosition)
-    {
-        if (inPrefab == null) return null;
+            _instance = this;
+            _settings = ProceduralInteriorSettings.TryGetSettings();
+            ReSeed();
+        }
 
-        GameObject newInterior = Instantiate(inPrefab, inPosition, Quaternion.identity);
+        [ContextMenu("Spawn Level")]
+        public void SpawnLevel()
+        {
+            if (Settings == null || _random == null) return;
 
-        return newInterior;
-    }
+            List<Interior> prefabs = Settings.InteriorPrefabs;
+            if (prefabs == null || prefabs.Count == 0) return;
 
-    public void ReSeed()
-    {
-        int seed = unchecked((int)DateTime.Now.Ticks);
-        _random = new System.Random(seed);
+            // Spawning single interior for now
+            int i = _random.Next(0, prefabs.Count);
+            SamplerHelperFunctions.GetRandomElement(prefabs, _random);
+            Interior prefab = prefabs[i];
+
+            Interior newInterior = SpawnInterior(prefab, Vector3.zero);
+            newInterior.SeedInterior(_random.Next());
+            newInterior.GenerateInterior(0);
+
+        }
+
+        private Interior SpawnInterior(Interior inPrefab, Vector3 inPosition)
+        {
+            if (inPrefab == null || _random == null) return null;
+
+            // apply random 90-degree rotation
+            float yRotate = (_random.Next() % 4) * 90.0f;
+            
+            Interior newInterior = Instantiate(inPrefab, inPosition, Quaternion.Euler(0.0f, yRotate, 0.0f));
+
+            return newInterior;
+        }
+
+        public void ReSeed()
+        {
+            int seed = unchecked((int)DateTime.Now.Ticks);
+            _random = new System.Random(seed);
+        }
     }
 }
